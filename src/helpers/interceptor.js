e8de1a87e6da367bbb6225f925e5af7d15c254c6
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-05-11 20:11:53 +0200

    fix

--- src/helpers/interceptor.js
+++ src/helpers/interceptor.js
@@ -5,6 +5,7 @@ import router from "../router";
 const setup = () => {
   axiosInstance.interceptors.request.use(
     (config) => {
+      if (config.url.includes("https")) return config;
       const token = JSON.parse(localStorage.getItem("token"));
       if (token && token.access_token) {
         config.headers["Authorization"] = "Bearer " + token.access_token;
@@ -22,7 +23,6 @@ const setup = () => {
     },
     async (err) => {
       const originalConfig = err.config;
-
       if (originalConfig.url !== "/api/user/login" && err.response) {
         // Access Token was expired
         if (err.response.status === 403 && !originalConfig._retry) {
